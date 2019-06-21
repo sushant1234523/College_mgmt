ff881cc9275be2af335f0d533839a5c2c009b767
diwashv1 <diwashv1@gmail.com>
2019-06-22 00:27:38 +0545

    reinitialized files

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace College\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        Schema::defaultStringLength(191);
+
+
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div>
+
+
+
+    </div>
+
+
+    <div class="container">
+        <table class="table" id="data-table">
+
+            <tr>
+                <th>Name</th>
+                <th>address</th>
+                <th>phn no</th>
+                <th>home</th>
+                <th>new</th>
+            </tr>
+
+            <tr>
+                <td>roshan</td>
+                <td>ngt</td>
+                <td>5132</td>
+                <td>dsdsd</td>
+                <td>ddd</td>
+            </tr>
+
+            <tr>
+                <td>roshan</td>
+                <td>ngt</td>
+                <td>5132</td>
+                <td>dsdsd</td>
+                <td>ddd</td>
+            </tr>
+            <tr>
+                <td>roshan</td>
+                <td>ngt</td>
+                <td>5132</td>
+                <td>dsdsd</td>
+                <td>ddd</td>
+            </tr>
+            <tr>
+                <td>roshan</td>
+                <td>ngt</td>
+                <td>5132</td>
+                <td>dsdsd</td>
+                <td>ddd</td>
+            </tr>
+
+
+        </table>
+
+    </div>
+
+
+@endsection
+
+
+@section('scripts')
+
+pt>
+
+    @endsection
